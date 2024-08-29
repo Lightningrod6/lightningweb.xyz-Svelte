5450893dc1038c756d8438880876ab13c779cb43
Lightning <lightningrod654@gmail.com>
2024-08-29 15:24:00 -0500

    Addded projects page and installed a fuck ton of packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,152 +24,11 @@
     onMount(() => {
         IntroAnimation();
     });
-    function aClicked() {
-        gsap.to('.cool-header', {
-            duration: 1,
-            text: ''
-        })
-
-        gsap.to('.projects-redirect', {
-            duration: 1,
-            text: ''
-        })
-            tl.to('.projects', {
-                duration: 1,
-                y: "-390px",
-            });
-            tl.to('.project-header', {
-                duration: 1,
-                text: 'Bot-Bot'
-            });
-            tl.to('.bot-bot', {
-                duration: 1,
-                text: 'Multi-purpose Discord bot'
-            });
-            tl.to('.go-back', {
-                duration: 1,
-                text: 'Go back'
-            }, '-=2')
-        }
-
-        function botBot() {
-            gsap.to('.project-header, .bot-bot', {
-                duration: 1,
-                text: ''
-            });
-            tl.to('.bot-bot-head', {
-                duration: 1,
-                text: 'Bot-Bot',
-                delay: 1
-                
-            })
-            .to('.bot-bot-head', {
-                duration: 1,
-                text: 'Bot-Bot: Multi-purpose Discord bot',
-                delay: 1
-            })
-            .to('.bot-bot-head', {
-                duration: 1,
-                y: "-390px",
-            })
-            .to('.bot-bot-link', {
-                duration: 1,
-                text: 'Check it out!'
-            })
-
-        }
-
-        function goBack() {
-            gsap.to('.project-header', {
-                duration: 1,
-                text: ''
-            });
-            gsap.to('.bot-bot', {
-                duration: 1,
-                text: ''
-            })
-
-            gsap.to('.bot-bot-head', {
-                duration: 1,
-                text: ''
-            })
-            gsap.to('.bot-bot-link', {
-                duration: 1,
-                text: ''
-            })
-            
-            gsap.to('.go-back', {
-                duration: 1,
-                text: ''
-            }).then(() => {
-                IntroAnimation();
-            })
-        }
-        function RedirectAnimationBotBot() {
-            gsap.to('.bot-bot-head', {
-                duration: 1,
-                text: ''
-            })
-            gsap.to('.bot-bot-link', {
-                duration: 1,
-                text: ''
-            })
-
-            gsap.to('.go-back', {
-                duration: 1,
-                text: ''
-            })
-
-            tl.to('body', {
-                duration: 1,
-                background: 'black'
-            })
-            gsap.fromTo('body', {
-                duration: 1,
-                backgroundImage: 'url(src/img/coolpicture.png)',
-                backgroundSize: 'cover',
-                backgroundPositionX: '1000px',
-                backgroundPositionY: '0px',
-            }, {
-                duration: 1,
-                backgroundPositionX: '0px',
-                backgroundRepeat: 'no-repeat'
-            })
-
-
-            tl.to('.redirection', {
-                duration: 1,
-                text: 'Redirecting...'
-            }).then(() => {
-                setInterval(() => {
-                    window.location.href = 'https://top.gg/bot/609076611609788427/';
-                }, 2000)
-            })
-
-        }
-
 </script>
 
 <style>
     .cool-header {
         margin-top: 400px;
-    }
-
-    .bot-bot-head {
-        margin-top: 800px;
-        left: 0;
-        right: 0;
-    }
-
-    .snap-center {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
-    .go-back {
-        bottom: 0;
-        position: absolute;
     }
     
 </style>
@@ -177,19 +36,7 @@
 
 
 <h1 class="text-center text-3xl cool-header"></h1>
-<div class="i-dont-know">
-    <a href="#" on:click={aClicked}><h1 class="text-2xl projects-redirect text-center"></h1></a>
-</div>
 
-<div class="projects">
-    <h1 class="text-center text-3xl project-header"></h1>
-    <div class="discord-bot">
-        <a href="#" on:click={botBot}><h1 class="text-2xl text-center bot-bot"></h1></a>
-        <div class="discord-bot-long">
-            <h1 class="text-3xl text-center bot-bot-head"></h1>
-            <a href="#" on:click={RedirectAnimationBotBot}><h3 class="text-center text-2xl bot-bot-link"></h3></a>
-            <h1 class="text-center text-2xl redirection"></h1>
-        </div>
-    </div>
-    <a href="#" on:click={goBack}><h3 class="absolute top-0 text-left go-back text-xl"></h3></a> <!-- Add 'absolute' and 'bottom-0' classes here -->
-</div>
+<a href="/projects"><h3 class="text-white text-center projects-redirect text-2xl"></h3></a>
+
+
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,65 @@
+<script lang='ts'>
+    import { Card } from 'flowbite-svelte';
+    import { gsap } from 'gsap'
+    import { TextPlugin } from 'gsap/all';
+    import { onMount } from 'svelte';
+
+    var tl = gsap.timeline();
+    gsap.registerPlugin(TextPlugin);
+
+    function IntroAnimation() {
+        tl.to('.projects-header', {
+            duration: 1,
+            text: 'Projects',
+        });
+
+        tl.from('.bot-bot-card, .wynncraft-search-card', {
+            duration: 0.8,
+            y: 50,
+            opacity: 0,
+            stagger: 0.2,
+            ease: 'power2.out',
+        }, '-=0.5'); // Start slightly before the previous animation ends
+    }
+
+    onMount(() => {
+        IntroAnimation();
+    })
+</script>
+
+<style>
+    .bot-bot-card {
+        padding-top: 400px;
+    }
+    .page-container {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+    .content {
+        flex: 1;
+    }
+    .go-back-button {
+        padding: 1rem;
+    }
+</style>
+
+<div class="page-container">
+    <div class="content">
+        <h1 class="text-center projects-header text-3xl"></h1>
+        <div class="projects bg-center flex justify-center items-center min-h-screen">
+            <Card href="https://top.gg/bot/609076611609788427" class="bg-gray-700 bot-bot-card m-4 hover:bg-gray-600 transition-colors duration-300 ease-in-out">
+                <h3 class="bot-bot-header text-white text-2xl text-center">Bot-Bot</h3>
+                <p class="text-white">Multipurpose moderation bot with a global economy</p>
+            </Card>
+            <Card class="bg-gray-700 wynncraft-search-card m-4" size="xs">
+                <h3 class="text-white text-2xl text-center wynncraft-header">Wynncraft Player Lookup</h3>
+                <p class="text-white">Search for players and guilds on the Wynncraft Minecraft server</p>
+            </Card>
+        </div>
+    </div>
+
+    <h3 class="go-back-button text-left">
+        <a href="/">Go Back</a>
+    </h3>
+</div>
