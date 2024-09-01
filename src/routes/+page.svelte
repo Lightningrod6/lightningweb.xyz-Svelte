f5cec89d0f1a66084c3c9a36ea3d5f8e16f6f993
Lightning <lightningrod654@gmail.com>
2024-09-01 13:24:49 -0500

    Added about me page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,6 +20,10 @@
             duration: 1,
             text: 'Projects'
         })
+        tl.to('.about-me-redirect', {
+            duration: 1,
+            text: 'About Me'
+        })
     }
     onMount(() => {
         IntroAnimation();
@@ -30,6 +34,12 @@
     .cool-header {
         margin-top: 400px;
     }
+
+    .redirects {
+        display: flex;
+        justify-content: center;
+        gap: 2rem; /* Adds space between the items */
+    }
     
 </style>
 
@@ -37,6 +47,7 @@
 
 <h1 class="text-center text-3xl cool-header"></h1>
 
-<a href="/projects"><h3 class="text-white text-center projects-redirect text-2xl"></h3></a>
-
-
+<div class="redirects">
+    <a href="/projects"><h3 class="text-white text-center projects-redirect text-2xl"></h3></a>
+    <a href="/about-me"><h3 class="text-white text-center about-me-redirect text-2xl"></h3></a>
+</div>
---
+++ src/routes/about-me/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import { gsap } from 'gsap';
+    import { TextPlugin } from 'gsap/all'
+    import { onMount } from 'svelte';
+    import { Card } from 'flowbite-svelte';
+   
+    gsap.registerPlugin(TextPlugin);
+   
+    onMount(() => {
+      var tl = gsap.timeline();
+      tl.to('.about-me-header', {
+        duration: 1,
+        text: 'About Me',
+      });
+    });
+   </script>
+   
+   <style>
+    .content {
+      position: relative;
+      width: 100%;
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: 1rem;
+      box-sizing: border-box;
+    }
+   
+    .description-1 {
+      width: 100%;
+      max-width: 400px;
+      text-align: center;
+    }
+   </style>
+   
+   <div class="content">
+    <h1 class="about-me-header text-3xl mb-4 text-white font-bold text-center"></h1>
+    <div class="description-1">
+      <h2 class="text-2xl mb-4 font-semibold">Hello, my name is Liam</h2>
+      <p class="mb-4 text-white">I am a college student currently trying to earn my bachelors in computer science. I have a passion for creating software, websites and hopefully ai products in the future.</p>
+    </div>
+   </div>
